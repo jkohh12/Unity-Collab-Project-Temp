a5adf623aedc8d7fe1efc19cad6578ff008b8317
jkohh12 <jkohh.12@gmail.com>
2023-07-06 17:49:38 -0700

    player can run up slopes, fixed physics/friction

--- Collab/Assets/Scripts/Player/PlayerMovement.cs
+++ Collab/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,21 +5,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    //--Component References
+
     private Rigidbody2D rb;
     private Animator animator;
-    private CircleCollider2D coll;
+    private CapsuleCollider2D coll;
     private SpriteRenderer sprite;
-    float directionX = 0f;
 
+
+    float directionX;
+
+    private Vector2 newVelocity;
 
     [SerializeField] private LayerMask jumpableGround;
-
 
     [SerializeField] private float moveSpeed;
 
     Vector2 vecGravity;
 
+    [Header("Friction")]
+    [SerializeField] private PhysicsMaterial2D noFriction;
+    [SerializeField] private PhysicsMaterial2D fullFriction;
+
+
+
+    [Header("Slope Check Variables")]
+    [SerializeField] private float slopeCheckDistance;
+    private Vector2 colliderSize;
+    private Vector2 slopeNormalPerp;
+    private float slopeDownAngle;
+    private float slopeDownAngleOld;
+    private bool isOnSlope;
+    private float slopeSideAngle;
+ 
+
+    [Header("Ground Check")]
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] private LayerMask whatIsGround;
+
+
     [Header("Jump Syatem")]
+    bool canJump;
+    bool isGrounded;
     [SerializeField] private float jumpForce;
     [SerializeField] private float jumpStartTime;
     [SerializeField] private float fallStartTime;
@@ -28,6 +57,9 @@ public class PlayerMovement : MonoBehaviour
     private float jumpCounter;
     [SerializeField] private float jumpMulitplier;
     [SerializeField] private float fallMultiplier;
+
+
+    //can implement max slope angle later if needed
 
     private bool isJumping;
 
@@ -44,20 +76,26 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
-        coll = GetComponent<CircleCollider2D>();
+        coll = GetComponent<CapsuleCollider2D>();
+
+        colliderSize = coll.size;
     }
 
     // Update is called once per frame
     private void Update()
     {
         directionX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(directionX * moveSpeed, rb.velocity.y);
-
-  
 
 
-        if (Input.GetButtonDown("Jump") && isGrounded())
+        //   rb.velocity = new Vector2(directionX * moveSpeed, rb.velocity.y);
+
+
+
+        if (Input.GetButtonDown("Jump") && canJump)
         {
+
+            
+            canJump = false;
             isJumping = true;
             jumpTime = jumpStartTime;
             fallTime = fallStartTime;
@@ -82,18 +120,22 @@ public class PlayerMovement : MonoBehaviour
             }
             else
             {
+             
                 isJumping = false;
             }
         }
 
         if (Input.GetButtonUp("Jump")) //if the user isn't holding space, then isJumping is false, no more increase in jumpforce
         {
+            
             isJumping = false;
         }
 
 
         if(rb.velocity.y < 0) // faster falling
         {
+
+            //isFalling = true;
             if (fallTime > 0)
             {
                 rb.velocity -= vecGravity * fallMultiplier * Time.deltaTime;
@@ -101,10 +143,82 @@ public class PlayerMovement : MonoBehaviour
             }
         }
   
-        UpdateAnimationState();
+
+
+   
 
     }
 
+    private void FixedUpdate()
+    {
+        CheckGround();
+        SlopeCheck();
+        ApplyMovement();
+        UpdateAnimationState();
+    }
+
+    private void SlopeCheck()
+    {
+        //determine position at bottom of capsule collider
+        Vector2 checkPos = coll.bounds.center - new Vector3(0.0f, colliderSize.y / 2);
+
+        SlopeCheckHorizontal(checkPos);
+        SlopeCheckVertical(checkPos);
+
+    }
+
+    private void SlopeCheckHorizontal(Vector2 checkPos)
+    {
+        RaycastHit2D slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeCheckDistance, whatIsGround);
+        RaycastHit2D slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeCheckDistance, whatIsGround);
+
+        if (slopeHitFront)
+        {
+            isOnSlope = true;
+            //slopeSideAngle = Vector2.Angle(slopeHitFront.normal, Vector2.up);
+        }
+
+        else if (slopeHitBack)
+        {
+            isOnSlope = true;
+           // slopeSideAngle = Vector2.Angle(slopeHitBack.normal, Vector2.up);
+        }
+        else
+        {
+            //slopeSideAngle = 0.0f;
+            isOnSlope = false;
+        }
+    }
+
+    private void SlopeCheckVertical(Vector2 checkPos)
+    {
+        //store what the vertical raycast hits
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, whatIsGround);
+
+        if (hit)
+        {
+            slopeNormalPerp = Vector2.Perpendicular(hit.normal).normalized;
+
+            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+            if(slopeDownAngle != slopeDownAngleOld)
+            {
+                isOnSlope = true;
+            }
+
+            slopeDownAngleOld = slopeDownAngle;
+            Debug.DrawRay(hit.point, slopeNormalPerp, Color.red);
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+        }
+
+        if(isOnSlope && directionX == 0.0f)
+        {
+            rb.sharedMaterial = fullFriction;
+        }
+        else
+        {
+            rb.sharedMaterial = noFriction;
+        }
+    }
     private void UpdateAnimationState()
     {
 
@@ -125,12 +239,12 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.idle;
         }
 
-        if(rb.velocity.y > 0.1f)
+        if (rb.velocity.y > 0.1f && !isOnSlope)
         {
             state = MovementState.jumping;
         }
 
-        else if(rb.velocity.y < -1.5f)
+        else if(rb.velocity.y < -1.5f && !isOnSlope)
         {
             state = MovementState.falling;
         }
@@ -138,9 +252,42 @@ public class PlayerMovement : MonoBehaviour
         animator.SetInteger("state", (int)state);
     }
 
-    private bool isGrounded()
+    private void ApplyMovement()
     {
-       return Physics2D.CircleCast(coll.bounds.center, 0.6925139f, Vector2.down, .1f, jumpableGround);
+        if (isGrounded && !isOnSlope && !isJumping)
+        {
+            newVelocity.Set(moveSpeed * directionX, 0.0f);
+            rb.velocity = newVelocity;
+        }
+        else if (isGrounded && isOnSlope && !isJumping)
+        {
+            newVelocity.Set(moveSpeed * slopeNormalPerp.x * -directionX, moveSpeed * slopeNormalPerp.y * -directionX);
+            rb.velocity = newVelocity;
+        }
+        else if (!isGrounded)
+        {
+            newVelocity.Set(moveSpeed * directionX, rb.velocity.y);
+            rb.velocity = newVelocity;
+        }
+    }
+
+    private void CheckGround()
+    {
+
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if(isGrounded && !isJumping)
+        {
+            canJump = true;
+        }
+        //return Physics2D.CircleCast(coll.bounds.center, 0.6925139f, Vector2.down, .1f, jumpableGround);
 
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
 }
